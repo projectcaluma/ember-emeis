26b57ce8511df5d814e8d1a17af7da9c0c8e89f3
Jonas Cosandey <jonas.cosandey@adfinis.com>
2020-06-25 07:59:32 +0200

    feat(routes): add basic route setup

--- addon/routes.js
+++ addon/routes.js
@@ -1,5 +1,22 @@
 import buildRoutes from "ember-engines/routes";
 
 export default buildRoutes(function () {
-  this.route("scopes", function () {});
+  this.route("users", function () {
+    this.route("edit", { path: "/:user_id" }, function () {
+      this.route("acl");
+    });
+    this.route("new");
+  });
+  this.route("scopes", function () {
+    this.route("edit", { path: "/:scope_id" });
+    this.route("new");
+  });
+  this.route("permissions", function () {
+    this.route("edit", { path: "/:permission_id" });
+    this.route("new");
+  });
+  this.route("roles", function () {
+    this.route("edit", { path: "/:role_id" });
+    this.route("new");
+  });
 });
