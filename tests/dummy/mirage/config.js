9b92cba914f89f9baee0748f782c55268896282f
Jonas Cosandey <jonas.cosandey@adfinis.com>
2020-06-25 07:59:32 +0200

    feat(routes): add basic route setup

--- tests/dummy/mirage/config.js
+++ tests/dummy/mirage/config.js
@@ -6,6 +6,35 @@ export default function () {
   this.get("/scopes");
   this.post("/scopes");
   this.get("/scopes/:id");
+  this.patch("/scopes/:id");
   this.put("/scopes/:id");
   this.del("/scopes/:id");
+
+  this.get("/permissions");
+  this.post("/permissions");
+  this.get("/permissions/:id");
+  this.patch("/permissions/:id");
+  this.put("/permissions/:id");
+  this.del("/permissions/:id");
+
+  this.get("/users");
+  this.post("/users");
+  this.get("/users/:id");
+  this.patch("/users/:id");
+  this.put("/users/:id");
+  this.del("/users/:id");
+
+  this.get("/roles");
+  this.post("/roles");
+  this.get("/roles/:id");
+  this.patch("/roles/:id");
+  this.put("/roles/:id");
+  this.del("/roles/:id");
+
+  this.get("/acls");
+  this.post("/acls");
+  this.get("/acls/:id");
+  this.patch("/acls/:id");
+  this.put("/acls/:id");
+  this.del("/acls/:id");
 }
