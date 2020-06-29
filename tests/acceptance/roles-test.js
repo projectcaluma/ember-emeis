f5bef3da7cc754f558c529890f486bd7e68fde49
Jonas Cosandey <jonas.cosandey@adfinis.com>
2020-06-29 10:20:10 +0200

    feat(roles): add edit view

--- tests/acceptance/roles-test.js
+++ tests/acceptance/roles-test.js
@@ -1,11 +1,20 @@
-import { visit, currentURL } from "@ember/test-helpers";
+import {
+  visit,
+  currentURL,
+  click,
+  fillIn,
+  waitUntil,
+} from "@ember/test-helpers";
 import { setupMirage } from "ember-cli-mirage/test-support";
 import { setupApplicationTest } from "ember-qunit";
 import { module, test } from "qunit";
 
+import setupRequestAssertions from "./../helpers/assert-request";
+
 module("Acceptance | roles", function (hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
+  setupRequestAssertions(hooks);
 
   test("list view /roles", async function (assert) {
     assert.expect(6);
@@ -25,5 +34,81 @@ module("Acceptance | roles", function (hooks) {
     assert
       .dom(`[data-test-role-name="${role.id}"] a`)
       .hasAttribute("href", `/roles/${role.id}`);
+  });
+
+  test("detail view /roles/:id", async function (assert) {
+    assert.expect(8);
+
+    const role = this.server.create("role");
+
+    await visit(`/roles/${role.id}`);
+
+    assert.equal(currentURL(), `/roles/${role.id}`);
+
+    assert.dom('[name="slug"]').hasValue(role.slug);
+    assert.dom('[name="slug"]').hasAttribute("disabled");
+    assert.dom('[name="name"]').hasValue(role.name.en);
+    assert.dom('[name="description"]').hasValue(role.description.en);
+
+    const name = "Role 1",
+      description = "The one and only";
+
+    await fillIn('[name="name"]', name);
+    await fillIn('[name="description"]', description);
+
+    this.assertRequest("PATCH", `/api/v1/roles/${role.id}`, (request) => {
+      const attributes = JSON.parse(request.requestBody).data.attributes;
+
+      assert.equal(attributes.name.en, name);
+      assert.equal(attributes.description.en, description);
+    });
+    await click("[data-test-save]");
+
+    await click("[data-test-back]");
+    assert.equal(currentURL(), "/roles");
+  });
+
+  test("delete /roles/:id", async function (assert) {
+    assert.expect(5);
+
+    const role = this.server.create("role", {
+      name: "test 1",
+      description: "this is test one.",
+    });
+
+    await visit(`/roles`);
+    assert.dom("[data-test-role-name]").exists({ count: 1 });
+
+    await click("[data-test-role-name] a");
+    assert.equal(currentURL(), `/roles/${role.id}`);
+
+    this.assertRequest("DELETE", `/api/v1/roles/:id`, (request) => {
+      assert.equal(role.id, request.params.id);
+    });
+    await click("[data-test-delete]");
+
+    // For some reason the await click is not actually waiting for the delete task to finish.
+    // Probably some runloop issue.
+    await waitUntil(() => currentURL() !== `/roles/${role.id}`);
+
+    assert.equal(currentURL(), `/roles?page=1`);
+    assert.dom("[data-test-role-name]").doesNotExist();
+  });
+
+  test("detail view /roles/:id permission table", async function (assert) {
+    assert.expect(4);
+
+    const role = this.server.create("role");
+    this.server.createList("permission", 10);
+
+    this.assertRequest("GET", `/api/v1/permissions`, (request) => {
+      assert.equal(request.queryParams["filter[role]"], role.id);
+    });
+
+    await visit(`/roles/${role.id}`);
+    assert.equal(currentURL(), `/roles/${role.id}`);
+
+    assert.dom("[data-test-permissions]").exists();
+    assert.dom("[data-test-permissions] [data-test-row]").exists({ count: 10 });
   });
 });
