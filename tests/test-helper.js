1a4016b0c3ae46d16f06f11e8be510ee2280ff18
Christian Zosel <christian@zosel.ch>
2021-08-25 10:48:54 +0200

    chore: run ember-cli-update (v3.22.0...v3.24.0)

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,9 +1,12 @@
 import { setApplication } from "@ember/test-helpers";
+import Application from "dummy/app";
+import config from "dummy/config/environment";
 import { start } from "ember-qunit";
-
-import Application from "../app";
-import config from "../config/environment";
+import * as QUnit from "qunit";
+import { setup } from "qunit-dom";
 
 setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
 
 start();
