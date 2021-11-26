c07a29cce427fcf70f46e78b0e3310c041de2697
Christian Zosel <christian@zosel.ch>
2021-11-26 14:45:10 +0100

    wip

--- addon/controllers/scopes/index.js
+++ addon/controllers/scopes/index.js
@@ -1,3 +1,25 @@
 import PaginationController from "ember-emeis/-private/controllers/pagination";
+import { action } from "@ember/object";
+import { inject as service } from "@ember/service";
 
-export default class ScopesIndexController extends PaginationController {}
+export default class ScopesIndexController extends PaginationController {
+  @service emeisOptions;
+  @service intl;
+  @service store;
+
+  get metaFields() {
+    return this.emeisOptions.metaFields?.scope;
+  }
+
+  @action
+  updateModel(model, formElements) {
+    model.name = formElements.name.value;
+    model.description = formElements.description.value;
+
+    return model;
+  }
+
+  get queryParamsfilter() {
+    return { scope: this.model.id };
+  }
+}
