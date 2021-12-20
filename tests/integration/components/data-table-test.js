5afa1d16968fc292b40815c7bb117628c8f86a97
Falk <falk.neumann@adfinis.com>
2021-12-20 16:51:21 +0100

    fix: remove args & fix testing

--- tests/integration/components/data-table-test.js
+++ tests/integration/components/data-table-test.js
@@ -1,4 +1,4 @@
-import { render, click, fillIn, settled } from "@ember/test-helpers";
+import { render, click, fillIn } from "@ember/test-helpers";
 import { hbs } from "ember-cli-htmlbars";
 import { setupMirage } from "ember-cli-mirage/test-support";
 import { setupIntl } from "ember-intl/test-support";
@@ -8,26 +8,13 @@ import { module, test } from "qunit";
 module("Integration | Component | data-table", function (hooks) {
   setupRenderingTest(hooks);
   setupMirage(hooks);
-  setupIntl(hooks);
+  setupIntl(hooks, "en");
 
   test("fetch and display correct data", async function (assert) {
-    assert.expect(11);
+    assert.expect(7);
 
     this.set("modelName", "role");
-
-    const store = this.owner.lookup("service:store");
-    store.query = (modelName) => {
-      assert.strictEqual(modelName, this.modelName);
-
-      const data = [
-        { name: "Role 1", slug: "role-1" },
-        { name: "Role 2", slug: "role-2" },
-      ];
-
-      data.meta = { pagination: { pages: 5 } };
-
-      return data;
-    };
+    const role = this.server.createList("role", 10)[0];
 
     await render(hbs`
       <DataTable @modelName={{this.modelName}} as |table|>
@@ -37,8 +24,9 @@ module("Integration | Component | data-table", function (hooks) {
         </table.head>
         <table.body as |body|>
           <body.row>
+            {{log body.model.name}}
             {{#let body.model as |role|}}
-              <td>{{role.name}}</td>
+              <td data-test-role-slug="{{role.slug}}">{{role.name}}</td>
               <td>{{role.slug}}</td>
             {{/let}}
           </body.row>
@@ -53,13 +41,10 @@ module("Integration | Component | data-table", function (hooks) {
     assert.dom("thead tr td:first-child").hasText("Heading 1");
     assert.dom("thead tr td:last-child").hasText("Heading 2");
 
-    assert.dom("tbody tr").exists({ count: 2 });
-
-    assert.dom("tbody tr:first-child td:first-child").hasText("Role 1");
-    assert.dom("tbody tr:first-child td:last-child").hasText("role-1");
-
-    assert.dom("tbody tr:last-child td:first-child").hasText("Role 2");
-    assert.dom("tbody tr:last-child td:last-child").hasText("role-2");
+    assert.dom("tbody tr").exists({ count: 10 });
+    assert
+      .dom(`tbody tr td[data-test-role-slug=${role.slug}]`)
+      .hasText(role.name.en);
   });
 
   test("pagination", async function (assert) {
@@ -120,72 +105,7 @@ module("Integration | Component | data-table", function (hooks) {
     assert.dom("tfoot li:last-child").hasClass("uk-disabled");
   });
 
-  test("fetches include string", async function (assert) {
-    assert.expect(5);
-
-    const users = this.set("modelName", "user");
-    this.server.createList("user", 4);
-
-    // const store = this.owner.lookup("service:store");
-    // store.query = (_, options) => {
-    //   assert.strictEqual(options.include, "acls.role");
-
-    //   const data = [
-    //     {
-    //       name: "User 1",
-    //       acls: {
-    //         role: {
-    //           id: "role-1",
-    //         },
-    //       },
-    //     },
-    //     {
-    //       name: "User 2",
-    //       acls: {
-    //         role: {
-    //           id: "role-2",
-    //         },
-    //       },
-    //     },
-    //   ];
-
-    //   data.meta = { pagination: { pages: 1 } };
-    //   return data;
-    // };
-
-    await render(hbs`
-      <DataTable
-        @modelName={{this.modelName}}
-        @include={{"acls.role"}}
-        as |table|>
-          <table.head as |head|>
-            <head.sortable @sort="one">
-              Heading One
-            </head.sortable>
-            <head.nonsortable>
-              Roles
-            </head.nonsortable>
-          </table.head>
-          <table.body as |body|>
-            <body.row>
-              {{#let body.model as |user|}}
-                <td>{{user.firstName}}</td>
-                <td>{{user.acls.role.name}}</td>
-              {{/let}}
-            </body.row>
-          </table.body>
-      </DataTable>
-    `);
-    await settled();
-
-    assert.dom("thead tr th:first-child").hasText("Heading One");
-    assert.dom("thead tr th:last-child").hasText("Roles");
-
-    assert.dom("tbody tr td:first-child").hasText("User 1");
-    assert.dom("tbody tr:first-child td:nth-child(2)").hasText("role-1");
-  });
-
-  test("fetches includes array", async function (assert) {
+  test("datatable fetches includes", async function (assert) {
     assert.expect(6);
 
     this.set("modelName", "user");
@@ -257,7 +177,7 @@ module("Integration | Component | data-table", function (hooks) {
     assert.dom("thead tr th:last-child").hasText("Scopes");
 
     assert.dom("tbody tr td:first-child").hasText("User 1");
-    assert.dom("tbody tr:last-child td:nth-child(2)").hasText("role-2");
+    assert.dom("tbody tr:last-child td:nth-child(2)").hasText("Role 2");
     assert.dom("tbody tr:last-child td:nth-child(3)").hasText("scope2");
   });
 
