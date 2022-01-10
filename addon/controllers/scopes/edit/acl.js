94686954e333e5c9cb858009b9d37cc18f9e036c
Falk <falk.neumann@adfinis.com>
2022-01-10 12:34:08 +0100

    feat(scopes): two column layout for scopes view

--- addon/controllers/scopes/edit/acl.js
+++
@@ -1,7 +0,0 @@
-import PaginationController from "ember-emeis/-private/controllers/pagination";
-
-export default class ScopesEditACLController extends PaginationController {
-  get queryParamsfilter() {
-    return { scope: this.model.id };
-  }
-}
--- addon/routes/scopes/edit/index.js
+++
@@ -1,7 +0,0 @@
-import Route from "@ember/routing/route";
-
-export default class ScopesEditIndexRoute extends Route {
-  model() {
-    return this.modelFor("scopes.edit");
-  }
-}
