3884c5e5662cdabcc7e32142fb343b647e01a222
Falk Neumann <derrabauke@users.noreply.github.com>
2022-01-26 17:18:15 +0100

    chore(deps): upgrade mirage (#388)
    * chore(deps): upgrade mirage
    
    * fix: use mirages createServer and fix other issues
    
    * chore: setup caching for test runs
    
    * fix: ember-engines resolutions
    
    * ci: add improved caching
    
    * chore: use explicit imports for faker

--- tests/dummy/mirage/config.js
+++ tests/dummy/mirage/config.js
@@ -1,40 +1,49 @@
-export default function () {
-  this.urlPrefix = "";
-  this.namespace = "/api/v1";
-  this.timing = 400;
+import { discoverEmberDataModels } from "ember-cli-mirage";
+import { createServer } from "miragejs";
 
-  this.get("/scopes");
-  this.post("/scopes");
-  this.get("/scopes/:id");
-  this.patch("/scopes/:id");
-  this.put("/scopes/:id");
-  this.del("/scopes/:id");
+export default function makeServer(config) {
+  return createServer({
+    ...config,
+    models: { ...discoverEmberDataModels(), ...config.models },
+    routes() {
+      this.urlPrefix = "";
+      this.namespace = "/api/v1";
+      this.timing = 400;
 
-  this.get("/permissions");
-  this.post("/permissions");
-  this.get("/permissions/:id");
-  this.patch("/permissions/:id");
-  this.put("/permissions/:id");
-  this.del("/permissions/:id");
+      this.get("/scopes");
+      this.post("/scopes");
+      this.get("/scopes/:id");
+      this.patch("/scopes/:id");
+      this.put("/scopes/:id");
+      this.del("/scopes/:id");
 
-  this.get("/users");
-  this.post("/users");
-  this.get("/users/:id");
-  this.patch("/users/:id");
-  this.put("/users/:id");
-  this.del("/users/:id");
+      this.get("/permissions");
+      this.post("/permissions");
+      this.get("/permissions/:id");
+      this.patch("/permissions/:id");
+      this.put("/permissions/:id");
+      this.del("/permissions/:id");
 
-  this.get("/roles");
-  this.post("/roles");
-  this.get("/roles/:id");
-  this.patch("/roles/:id");
-  this.put("/roles/:id");
-  this.del("/roles/:id");
+      this.get("/users");
+      this.post("/users");
+      this.get("/users/:id");
+      this.patch("/users/:id");
+      this.put("/users/:id");
+      this.del("/users/:id");
 
-  this.get("/acls");
-  this.post("/acls");
-  this.get("/acls/:id");
-  this.patch("/acls/:id");
-  this.put("/acls/:id");
-  this.del("/acls/:id");
+      this.get("/roles");
+      this.post("/roles");
+      this.get("/roles/:id");
+      this.patch("/roles/:id");
+      this.put("/roles/:id");
+      this.del("/roles/:id");
+
+      this.get("/acls");
+      this.post("/acls");
+      this.get("/acls/:id");
+      this.patch("/acls/:id");
+      this.put("/acls/:id");
+      this.del("/acls/:id");
+    },
+  });
 }
