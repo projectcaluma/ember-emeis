67e6401b2972c73daeb0ea00a4dbb61dc53b64e3
Falk Neumann <derrabauke@users.noreply.github.com>
2022-07-04 12:40:28 +0200

    feat(users): add 'isActive' filter radio buttons (#510)
    * feat(users): add 'isActive' filter radio buttons
    
    * fix(data-table): optional check for sort queryParam

--- addon/controllers/users/index.js
+++ addon/controllers/users/index.js
@@ -1,4 +1,6 @@
+import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
+import { tracked } from "@glimmer/tracking";
 import { task } from "ember-concurrency";
 
 import { confirmTask } from "../../decorators/confirm-task";
@@ -10,6 +12,8 @@ export default class UsersIndexController extends PaginationController {
   @service emeisOptions;
   @service notification;
   @service intl;
+
+  @tracked filter_active = true;
 
   get customColumns() {
     return this.emeisOptions.user?.customColumns;
@@ -25,6 +29,17 @@ export default class UsersIndexController extends PaginationController {
 
   get linkToScope() {
     return this.emeisOptions.navigationEntries?.includes("scopes");
+  }
+
+  get filters() {
+    return { isActive: this.filter_active };
+  }
+
+  @action
+  updateFilter(type, eventOrValue) {
+    this[`filter_${type}`] = eventOrValue?.target?.value ?? eventOrValue;
+    // reset page filter
+    this.page = 1;
   }
 
   @task
