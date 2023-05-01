92071a83a821e64da61c17499ee8a6427fad95f7
Falk <falk.neumann@adfinis.com>
2023-05-01 13:09:50 +0200

    chore(deps): update @adfinis/eslint-config and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
   settings: {
     "import/internal-regex": "^ember-emeis/",
   },
-  extends: ["@adfinis-sygroup/eslint-config/ember-addon"],
+  extends: ["@adfinis/eslint-config/ember-addon"],
   rules: {
     "ember/no-settled-after-test-helper": "warn",
   },
