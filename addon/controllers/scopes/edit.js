12a371371b06279975ba5f73191cdf14c1b1881f
Falk <falk.neumann@adfinis.com>
2023-05-01 10:53:04 +0200

    fix(scopes): prevent cyclic relation selection
    Filter the current scope so it can't get selected as it's
    own parent scope.

--- addon/controllers/scopes/edit.js
+++ addon/controllers/scopes/edit.js
@@ -1,13 +1,17 @@
 import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
+import { localCopy } from "tracked-toolbox";
 
 import PaginationController from "ember-emeis/-private/controllers/pagination";
 
 export default class ScopesEditIndexController extends PaginationController {
   @service emeisOptions;
+  @service notification;
   @service intl;
   @service router;
   @service store;
+
+  @localCopy("model.parent") parent;
 
   get queryParamsfilter() {
     return { scope: this.model?.id };
@@ -17,20 +21,42 @@ export default class ScopesEditIndexController extends PaginationController {
     return this.emeisOptions.scope?.metaFields;
   }
 
-  get allScopes() {
-    return this.store.peekAll("scope");
+  get childScopeIds() {
+    return this.model?.children.map((s) => s.id) ?? [];
+  }
+
+  get allOtherScopes() {
+    return this.store
+      .peekAll("scope")
+      .filter(
+        (scope) =>
+          scope.id !== this.model?.id && !this.childScopeIds.includes(scope.id)
+      );
   }
 
   @action
   updateModel(model, formElements) {
     model.name = formElements.name.value;
     model.description = formElements.description.value;
+    if (
+      this.parent
+        .findParents()
+        .map((scope) => scope.id)
+        .includes(this.model.id)
+    ) {
+      this.parent = model.parent;
+      this.notification.warning(
+        this.intl.t("emeis.form.invalid-scope-assignment-warning")
+      );
+    } else {
+      model.parent = this.parent;
+    }
 
     return model;
   }
 
   @action
   setParent(scope) {
-    this.model.parent = scope;
+    this.parent = scope;
   }
 }
