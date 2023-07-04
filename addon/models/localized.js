beab07336f4f34a7b1304456657df312140ef692
Falk <falk.neumann@adfinis.com>
2023-07-04 17:43:24 +0200

    chore(deps): pachtes and minor updates

--- addon/models/localized.js
+++ addon/models/localized.js
@@ -1,5 +1,5 @@
-import Model from "@ember-data/model";
 import { inject as service } from "@ember/service";
+import Model from "@ember-data/model";
 
 export default class LocalizedModel extends Model {
   @service intl;
