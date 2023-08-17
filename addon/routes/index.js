17ea0945e838df0c22deca420609a1b277cc5748
Mitan Omar <mitan@gmx.ch>
2023-08-17 16:58:04 +0200

    fix: mount point of the package

--- addon/routes/index.js
+++ addon/routes/index.js
@@ -1,10 +1,13 @@
-import Route from "@ember/routing/route";
-import { service } from "@ember/service";
+import { getOwner } from '@ember/application';
+import Route from '@ember/routing/route';
+import { service } from '@ember/service';
 
 export default class IndexRoute extends Route {
   @service hostRouter;
 
   beforeModel() {
-    this.hostRouter.transitionTo("ember-emeis.users");
+    const owner = getOwner(this);
+    const mountPoint = owner.mountPoint;
+    this.hostRouter.transitionTo(`${mountPoint}.users`);
   }
 }
