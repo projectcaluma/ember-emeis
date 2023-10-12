2b53e453f7d812d2457710db69fa51ffbe7e9139
Christian Zosel <christian.zosel@adfinis-sygroup.ch>
2023-10-12 18:30:17 +0200

    feat(user): filter export (#722)

--- addon/controllers/users.js
+++ addon/controllers/users.js
@@ -1,24 +1,8 @@
 import Controller from "@ember/controller";
 import { inject as service } from "@ember/service";
-import { task } from "ember-concurrency";
-import { saveAs } from "file-saver";
-
-import { handleTaskErrors } from "ember-emeis/-private/decorators";
 
 export default class UsersController extends Controller {
   @service notification;
   @service intl;
   @service fetch;
-
-  @task
-  @handleTaskErrors
-  *export() {
-    const response = yield this.fetch.fetch(`/api/v1/users/export`);
-
-    const filename = response.headers
-      .get("content-disposition")
-      .match(/filename="(.*)"/)[1];
-
-    saveAs(yield response.blob(), filename);
-  }
 }
