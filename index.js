7691ab03ae03f8d7d07c6f46bd4d19bae8bc78c3
Christian Zosel <christian@zosel.ch>
2025-10-13 11:22:51 +0200

    chore: upgrade dependencies

--- index.js
+++ index.js
@@ -7,4 +7,7 @@ const { buildEngine } = require("ember-engines/lib/engine-addon");
 module.exports = buildEngine({
   name: require("./package").name,
   lazyLoading: { enabled: false },
+  babel: {
+    plugins: [require.resolve("ember-concurrency/async-arrow-task-transform")],
+  },
 });
