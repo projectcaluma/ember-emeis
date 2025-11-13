c4abaa707671aa3ac05a724e8f53e83c6d4d41c8
Mitan Omar <mitan@gmx.ch>
2025-11-13 11:38:55 +0100

    fix: linting issues

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,5 +1,9 @@
 "use strict";
 
 module.exports = {
+  customSyntax: "postcss-scss",
   extends: ["stylelint-config-standard", "stylelint-prettier/recommended"],
+  rules: {
+    "import-notation": null,
+  },
 };
